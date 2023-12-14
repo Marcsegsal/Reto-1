fb5e5eb7dbde044b30f537e6c5478ced23a43967
vmmarmanica@alu.edu.gva.es <marmanica@alu.edu.gva.es>
2023-12-14 17:35:47 +0100

    peticio y usuario

--- ModuloUsuario.java
+++ ModuloUsuario.java
@@ -4,18 +4,25 @@ import java.util.Scanner;
 
 public class ModuloUsuario {
     static Scanner scanner = new Scanner(System.in);
-    static ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
+    static Usuario usuarioActual;
+    static ArrayList<Usuario> usuarios = new ArrayList<>();
+    static ArrayList<Peticion> peticiones = new ArrayList<>();
     public static void main(String[] args) {
         int eleccionMenu;
 
         do {
             cargarDatosUsuario();
+            cargarDatosCategorias();
+            cargarDatosPeticiones();
             mostrarMenu();
             eleccionMenu = scanner.nextInt();
 
             if (eleccionMenu < 0 || eleccionMenu > 3) {
                 System.out.println("El número introducido no es válido, por favor introduce otro número");
             }
+            if (eleccionMenu == 0)
+                guardarDatosPeticiones();
+
             switch (eleccionMenu) {
                 case 1:
                     generarPeticion();
@@ -40,7 +47,8 @@ public class ModuloUsuario {
         System.out.println("3-Consultar la petición");
     }
 
-    public static void identificarse() {}
+    public static void identificarse() {
+    }
 
     public static void cargarDatosUsuario() {
         try {
@@ -53,21 +61,63 @@ public class ModuloUsuario {
                 usuarios.add(new Usuario((Integer.parseInt(palabras[0])), palabras[1], palabras[2]));
                 linea = f_ent.readLine();
             }
+            f_ent.close();
 
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
     }
 
-    public static void cargarDatosPeticiones() {}
+    public static void cargarDatosPeticiones() {
+        try {
+            BufferedReader f_ent = new BufferedReader(new FileReader(new File("./CSV/peticion.csv")));
+            String linea = f_ent.readLine();
+            linea = f_ent.readLine();
+
+            while (linea != null) {
+                String[] palabras = linea.split(",");
+                peticiones.add(new Peticion((Integer.parseInt(palabras[0])), Integer.parseInt(palabras[1]), palabras[2], palabras[3], Integer.parseInt(palabras[4]), Integer.parseInt(palabras[5]), Integer.parseInt(palabras[6]), stringToBoolean(palabras[7])));
+            }
+            f_ent.close();
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
     public static void cargarDatosCategorias() {}
 
-    public static void guardarDatosPeticiones() {}
+    public static void guardarDatosPeticiones() {
+        try {
+            PrintWriter f_sal = new PrintWriter(new FileWriter("./CSV/peticion.csv"));
+            Peticion peticion;
+
+            for (int i = 0; i < peticiones.size(); i++) {
+                peticion = peticiones.get(i);
+                f_sal.println(peticion.getId() + "," + peticion.getIdUsuario() + "," + peticion.getDescripcion() + "," + peticion.getFecha() + "," + peticion.getIdCategoria() + "\n");
+            }
+            f_sal.close();
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
     public static void generarPeticion() {}
 
     public static void consultarPeticion() {}
 
-    public static void modificarDescripcion() {}
+    public static void modificarDescripcion() {
+
+    }
+
+    public static boolean stringToBoolean(String s) {
+        boolean b;
+        if (s.equals("true")) {
+            b = true;
+        } else
+            b = false;
+
+        return b;
+    }
 }
